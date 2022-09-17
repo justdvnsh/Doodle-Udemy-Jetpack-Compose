aa5303bd67344e5279452adfcc1bcd0fa7013ee6
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2022-09-17 22:07:37 +0530

    added the starting project

--- app/src/main/java/divyansh/tech/doodle/MainActivity.kt
+++ app/src/main/java/divyansh/tech/doodle/MainActivity.kt
@@ -3,21 +3,115 @@ package divyansh.tech.doodle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import divyansh.tech.doodle.ui.theme.DoodleTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DoodleTheme {
 
+                val viewModel = MainViewModel()
+
+                val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(bottomSheetState =
+                    BottomSheetState(BottomSheetValue.Collapsed)
+                )
+
+                BoxWithConstraints {
+                    BottomSheetScaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(text = "Doodle")
+                                }
+                            )
+                        },
+                        sheetElevation = 4.dp,
+                        sheetContent = {
+                            BottomSheetContent(
+                                viewModel = viewModel,
+                                onColorSelected = {
+                                    viewModel.onColorSelect(it)
+                                },
+                                onStrokeSelected = {
+                                    viewModel.onStrokeWidthSelect(it)
+                                }
+                            )
+                        },
+                        sheetBackgroundColor = MaterialTheme.colors.background,
+                        sheetShape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp),
+                        scaffoldState = bottomSheetScaffoldState
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomSheetContent(
+    viewModel: MainViewModel,
+    onColorSelected: (selectedColor: Color) -> Unit = {},
+    onStrokeSelected: (selectedStrokeWidth: Dp) -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(20.dp),
+    ) {
+        Text(text = "Colors", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+        LazyRow(
+            contentPadding = PaddingValues(16.dp),
+        ) {
+            items(viewModel.colorList) { item ->
+                Box(modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .size(24.dp)
+                    .background(
+                        color = item,
+                        shape = RoundedCornerShape(50.dp)
+                    )
+                    .clickable(onClick = { onColorSelected(item) })
+                )
+            }
+        }
+
+        Text(text = "Stroke Width", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+        LazyRow(
+            contentPadding = PaddingValues(24.dp)
+        ) {
+            items(viewModel.strokeWidthList) { item ->
+                Box(modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .size(item)
+                    .background(
+                        color = Color.Black,
+                        shape = RoundedCornerShape(50.dp)
+                    )
+                    .clickable(onClick = { onStrokeSelected(item) })
+                )
             }
         }
     }
---
+++ app/src/main/java/divyansh/tech/doodle/MainViewModel.kt
@@ -0,0 +1,47 @@
+package divyansh.tech.doodle
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    private val _colorList = mutableStateListOf<Color>()
+    val colorList: List<Color> = _colorList
+
+    private val _strokeWidthList = mutableStateListOf<Dp>()
+    val strokeWidthList: List<Dp> = _strokeWidthList
+
+    var selectedColor by mutableStateOf(Color.Black)
+
+    var selectedStrokeWidth by mutableStateOf(6.dp)
+
+    init {
+        _colorList.add(Color.Black)
+        _colorList.add(Color.Gray)
+        _colorList.add(Color.Green)
+        _colorList.add(Color.Red)
+        _colorList.add(Color.Blue)
+        _colorList.add(Color.Yellow)
+        _colorList.add(Color.Cyan)
+        _colorList.add(Color.Magenta)
+
+        for (i in 6 until 26 step 2) {
+            _strokeWidthList.add(i.dp)
+        }
+    }
+
+    fun onColorSelect(color: Color) {
+        selectedColor = color
+    }
+
+    fun onStrokeWidthSelect(width: Dp) {
+        selectedStrokeWidth = width
+    }
+}
